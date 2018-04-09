e6f67e0674709f731ce1fec71350d9fb5d4b34ff
mpro34 <mpro34@gmail.com>
2018-04-08 17:15:50 -0700

    add purchase and rental classes

--- real_estate_app/house.py
+++ real_estate_app/house.py
@@ -1,2 +1,33 @@
+from utils import get_valid_input as validate
+
 class House(Property):
-    pass
+    valid_garage = [ "attached", "detached", "none" ]
+    valid_fenced = [ "yes", "no" ]
+
+    def __init__(self, num_stories='', garage='', fenced='', **kwargs):
+        super().__init__(**kwargs)
+        self.garage = garage
+        self.fenced = fenced
+        self.num_stories = num_stories
+
+    def display(self):
+        print("House Super = ", super())
+        super().display()
+        print("HOUSE DETAILS")
+        print("# of stories: {}".format(self.num_stories))
+        print("garage: {}".format(self.garage))
+        print("fenced yard: {}".format(self.fenced))
+
+    def prompt_init():
+        parent_init = Property.prompt_init()
+        fenced = validate("Is the yard fenced? ", House.valid_fenced)
+        garage = validate("Is there a garage? ", House.valid_garage)
+        num_stories = input("How many stories? ")
+
+        parent_init.update({
+            "fenced": fenced,
+            "garage": garage,
+            "num_stories": num_stories
+        })
+        return parent_init
+    prompt_init = staticmethod(prompt_init)
--- real_estate_app/purchase.py
+++ real_estate_app/purchase.py
@@ -1,2 +1,18 @@
 class Purchase:
-    pass
+    def __init__(Self, price='', taxes='', **kwargs):
+        super().__init__(**kwargs)
+        self.price = price
+        self.taxes = taxes
+
+    def display(self):
+        super().display()
+        print("PURCHASE DETAILS")
+        print("selling price: {}".format(self.price))
+        print("estimated taxes: {}".format(self.taxes))
+
+    def prompt_init():
+        return dict(
+            print=input("What is the selling price? "),
+            taxes=input("What are the estimated taxes? ")
+        )
+    prompt_init = staticmethod(prompt_init)
